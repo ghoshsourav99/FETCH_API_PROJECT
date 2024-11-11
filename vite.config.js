04995a247e688930eb63adba69d115cab21a7462
ghoshsourav99 <11215145sourav@gmail.com>
2024-11-11 16:05:29 +0530

    update various things to fixed vite error

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/FetchApiProject/',  // Replace 'my-repo-name' with your actual repo name
-  // other Vite configurations
-})
+export default {
+  base: '/Fetch_Api_Project/', // Replace 'Fetch_Api_Project' with your repo name
+}
+
